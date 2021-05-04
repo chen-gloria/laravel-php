0af753cedaeb7d3987b66e9aee4ec8d592be07af
Rouyu Chen <u6539353@anu.edu.au>
2021-05-04 16:07:32 +1000

    Pratice some raw SQL Queries in Laravel routers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,9 +19,49 @@ use App\Http\Controllers\TestController;
 //     return view('welcome');
 // });
 
-Route::get('/test', [TestController::class, 'index']);
-Route::get('/contact', [TestController::class, 'contact']);
-Route::get('test/{id}/{name}', [TestController::class, 'show_test']); // Doesn't matter what is the name inside {}
+// Route::get('/test', [TestController::class, 'index']);
+// Route::get('/contact', [TestController::class, 'contact']);
+// Route::get('test/{id}/{name}', [TestController::class, 'show_test']); // Doesn't matter what is the name inside {}
 
 // Route::resource('tests', TestController::class);
 
+
+// Database Raw SQL Queries
+
+Route::get('/insert', function(){
+// Facade
+
+    DB::insert('insert into test(title, content, is_admin) values(?,?,?)', 
+                ['PHP with Laravel', 'Second content', 1]);
+});
+
+
+Route::get('/read', function() {
+
+    $results = DB::select('select * from test where id = ?', [1]); // An array of results, STD class object
+    return var_dump($results);
+    
+    foreach($results as $result) {
+        // return($result->title);
+    }
+});
+
+Route::get('/update', function(){
+
+    $updated = DB::update('update test set title = "Update title" where id = ?', [1]);
+
+    return $updated; // return 1
+
+});
+
+Route::get('/delete', function(){
+
+    $deleted = DB::delete('delete from test where id = ?', [3]);
+
+    if ($deleted == 1) {
+        return "It's successfully deleted!";
+    } else {
+        return "It's repeated deletion.";
+    }
+
+});
