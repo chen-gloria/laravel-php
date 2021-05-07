52ba71de2ce2705115e620f9f1d01a184d7e3801
Rouyu Chen <u6539353@anu.edu.au>
2021-05-07 15:32:47 +1000

    Part of basic Laravel Eloquent database functions

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\DB;
+use App\Models\Test;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,40 +29,91 @@ use Illuminate\Support\Facades\DB;
 
 // Database Raw SQL Queries
 
-Route::get('/insert', function(){
-// Facade
+// Route::get('/insert', function(){
 
-    DB::insert('insert into test(title, content, is_admin) values(?,?,?)', 
-                ['PHP with Laravel', 'Second content', 1]);
-});
+//     DB::insert('insert into test(title, content, is_admin) values(?,?,?)', 
+//                 ['PHP with Laravelaaaa', 'Second content Yes', 0]);
+// });
 
 
-Route::get('/read', function() {
+// Route::get('/read', function() {
 
-    $results = DB::select('select * from test where id = ?', [1]); // An array of results, STD class object
-    return var_dump($results);
+//     $results = DB::select('select * from test where id = ?', [1]); // An array of results, STD class object
+//     return var_dump($results);
     
-    foreach($results as $result) {
-        // return($result->title);
-    }
-});
+//     foreach($results as $result) {
+//         // return($result->title);
+//     }
+// });
 
-Route::get('/update', function(){
+// Route::get('/update', function(){
 
-    $updated = DB::update('update test set title = "Update title" where id = ?', [1]);
+//     $updated = DB::update('update test set title = "Update title" where id = ?', [1]);
 
-    return $updated; // return 1
+//     return $updated; // return 1
 
-});
+// });
 
-Route::get('/delete', function(){
+// Route::get('/delete', function(){
 
-    $deleted = DB::delete('delete from test where id = ?', [3]);
+//     $deleted = DB::delete('delete from test where id = ?', [3]);
 
-    if ($deleted == 1) {
-        return "It's successfully deleted!";
-    } else {
-        return "It's repeated deletion.";
-    }
+//     if ($deleted == 1) {
+//         return "It's successfully deleted!";
+//     } else {
+//         return "It's repeated deletion.";
+//     }
 
+// });
+
+// ELOQUENT, Object Relationship Model (ORM)
+
+// Route::get('/read', function() {
+
+//     $tests = Test::all();
+
+//     foreach($tests as $test) {
+//         return $test->title;
+//     }
+
+// });
+
+// Route::get('/findwhere', function() {
+
+//     $tests = Test::orderBy('id', 'desc')->take(3)->get();
+//     return $tests;
+// });
+
+// Route::get('/findwhere', function() {
+
+//     $tests = Test::findOrFail(10);
+
+//     return $tests;
+// });
+
+// Route::get('/findwhere', function() {
+
+//     $tests = Test::where('users_count', '<', 50)->firstOrFail();
+
+//     return $tests;
+// });
+
+// Route::get('/basicinsert', function() {
+
+//     $test = new Test; //Instantiate
+
+//     $test->title = 'new ORM Title';
+//     $test->content = 'New New New content';
+
+//     $test->save();
+// });
+
+Route::get('/basicinsert', function() {
+
+    $test = Test::find(1); //Instantiate
+
+    $test->title = 'new ORM Title';
+    $test->content = 'New New New content';
+
+    $test->save();
 });
